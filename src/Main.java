4cad96ee225e0a75df3d46fd341063ecc4c52ee7
Dasha <d.5ysueva@yandex.ru>
2025-04-03 09:08:47 +0300

    а

--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,23 @@ import java.util.regex.*;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Введите кол-во IP-адресов");
+        Scanner n1 = new Scanner(System.in);
+        int n = n1.nextInt();
         System.out.println("Введите строчку для проверки IP-адреса ");
         Scanner in = new Scanner(System.in);
-        String input = in.nextLine();
-
-        Pattern p =Pattern.compile("(^(25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d\\.){3}(25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)$)");
-        Matcher m = p.matcher(input);
-        if(m.find())
-        {
-            System.out.println("Вы ввели IP-адрес");
-
+        String[] input = new String[n];
+        for (int i = 0; i < n; i++) {
+            input[i] = in.nextLine();
         }
-        else {
-            System.out.println("Вы ввели не IP-адрес");
+        String sample = "^((25[0-5]|2[0-4][0-9]|1\\d\\d|[1-9]?\\d)\\.){3}(25[0-5]|2[0-4][0-9]|1\\d\\d|[1-9]?\\d)$";
+        Pattern pattern = Pattern.compile(sample);
+        System.out.println("Найденные IP-адреса");
+        for (String Sample : input) {
+            Matcher mat = pattern.matcher(Sample);
+            if(mat.matches()){
+                System.out.println(Sample);
+            }
         }
     }
 }
